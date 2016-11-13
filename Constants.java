f88a2d12d5bfb5fb91e864b1214a146f9230e9bf
ambtomagos <ambtomagos@gmail.com>
2016-11-13 18:35:21 +0800

    pasearch po ng @UITEAM para malaman ang mga babaguhin :)

---
+++ Constants.java
@@ -0,0 +1,34 @@
+public interface Constants {
+	/**
+	 * Number of rows and columns of the game grid
+	 */
+	public static final int ROWS=5;
+	public static final int COLS=5;
+
+	/**
+	 * Change number of players here
+	 */
+	public final int numplayers = 3;
+
+    public final int SEC = 5; // number of seconds buttons will be disabled when correctly matched
+    public final int TIME_LIMIT = 1; //number of minutes the timer for each level will last
+
+    /**
+	 * Game states.
+	 */
+	public static final int GAME_START=0;
+	public static final int IN_PROGRESS=1;
+	public final int GAME_END=2;
+	public final int WAITING_FOR_PLAYERS=3;
+
+	/**
+	 * Ports
+	 */
+	public final int TCP_PORT = 60010;
+	public final int UDP_PORT = 60011;
+
+
+
+	String SERVER_ADDRESS="192.168.56.1";
+
+}
